6175d323e86c45f137e35239b52a3e60e33136bb
ㅁ <>
2022-05-29 11:25:09 +0900

    연산자

---
+++ py12.py
@@ -0,0 +1,14 @@
+from sympy import N
+
+
+price = 50000
+n = int(input('할부 개월 입력>>>'))
+print('매달 내는 돈은 {}원입니다.'.format(50000/n))
+var1 = float(input("첫 번째 실수를 입력하세요>>>"))
+var2 = float(input("두 번째 실수를 입력하세요>>>"))
+print(var1)
+print(var2)
+print('{}와 {}의 합은 {}입니다.'.format(var1,var2,var1 + var2))
+
+
+
---
+++ py13.py
@@ -0,0 +1,17 @@
+# page 73
+a = 7
+b = 2
+print('{}+{} = {}'.format(a, b, a+b))       # 더하기
+print('{}+{} = {}'.format(a, b, a-b))       # 빼기
+print('{}+{} = {}'.format(a, b, a*b))       # 곱하기
+print('{}+{} = {}'.format(a, b, a**b))      # 제곱승
+print('{}+{} = {}'.format(a, b, a/b))       # 나누기(소수점까지)
+print('{}+{} = {}'.format(a, b, a//b))      # 나누기(소수점 뺌)
+print('{}+{} = {}'.format(a, b, a%b))       # 나머지 구하기
+
+# page 85 (1)
+변수1 = input('10 ~ 99사이의 정수를 입력하세요>>>' )
+print('십의 자리: {}'.format(변수1//10))
+print('일의 자리: {}'.format(변수1%10))
+
+
